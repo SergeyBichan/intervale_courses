e385b82ab042b8997f597d4483440e231a44416b
SergeyBichan <ugidancer@gmail.com>
2022-12-24 20:07:17 +0300

    gitignore .idea/

--- src/ru/intervale/courses/task1/Runner.java
+++ src/ru/intervale/courses/task1/Runner.java
@@ -6,15 +6,18 @@ import java.util.Objects;
 public class Runner {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
-        int[] emptyArray = null;
+        int[] arr2 = new int[]{0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14};
+        int[] emptyArray = new int[]{};
 
-        System.out.println(Arrays.toString(numOfPositiveAndSumOfNegative(arr)));
-        System.out.println(Arrays.toString(numOfPositiveAndSumOfNegative(emptyArray)));
+        System.out.println(Arrays.toString(numOfPositiveAndSumOfNegativeByCycle(arr)));
+        System.out.println(Arrays.toString(numOfPositiveAndSumOfNegativeByCycle(emptyArray)));
+
+        System.out.println(Arrays.toString(numOfPositiveAndSumOfNegativeByCycle(arr2)));
 
 
     }
 
-    public static int[] numOfPositiveAndSumOfNegative(int[] array) {
+    public static int[] numOfPositiveAndSumOfNegativeByCycle(int[] array) {
         int sumOfPositive = 0;
         int sumOfNegativeNumbers = 0;
 
@@ -28,7 +31,7 @@ public class Runner {
                 }
             }
         }
-        if (Objects.isNull(array)) {
+        if (Objects.isNull(array) || array.length == 0) {
             return new int[0];
         }
         return new int[]{sumOfPositive, sumOfNegativeNumbers};
