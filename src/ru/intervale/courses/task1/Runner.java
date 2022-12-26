a543326893ef70b5ca0963e2cb235f785abb66ba
SergeyBichan <ugidancer@gmail.com>
2022-12-26 14:05:38 +0300

    add some descriptions

--- src/ru/intervale/courses/task1/Runner.java
+++ src/ru/intervale/courses/task1/Runner.java
@@ -7,13 +7,17 @@ public class Runner {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
         int[] arr2 = new int[]{0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14};
-        int[] emptyArray = new int[]{};
+        int[] emptyArray = null;
 
+        System.out.println("Циклом");
         System.out.println(Arrays.toString(numOfPositiveAndSumOfNegativeByCycle(arr)));
+        System.out.println(Arrays.toString(numOfPositiveAndSumOfNegativeByCycle(arr2)));
         System.out.println(Arrays.toString(numOfPositiveAndSumOfNegativeByCycle(emptyArray)));
 
-        System.out.println(Arrays.toString(numOfPositiveAndSumOfNegativeByCycle(arr2)));
 
+        System.out.println("Стримом");
+        System.out.println(Arrays.toString(numOfPositiveAndSumOfNegativeByStream(arr)));
+        System.out.println(Arrays.toString(numOfPositiveAndSumOfNegativeByStream(arr2)));
         System.out.println(Arrays.toString(numOfPositiveAndSumOfNegativeByStream(emptyArray)));
 
 
